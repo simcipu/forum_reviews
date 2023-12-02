e391429abe6f0093a8e9a9c1473825ed16e93ae7
simone cipullo <s.cipullo@innovationengineering.eu>
2023-12-03 00:41:50 +0100

    first commit

---
+++ forum_review/forum_review/src/main/java/it/forum_review/ForumReviewApplication.java
@@ -0,0 +1,13 @@
+package it.forum_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumReviewApplication.class, args);
+	}
+
+}
